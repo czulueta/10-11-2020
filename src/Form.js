f3c564115270d1cac64da1d29d29f7e5e2ee39c7
czulueta <https://github.com/czulueta/weed.git>
2020-10-14 10:29:07 -0600

    updated form component

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,26 @@
 import React from "react"
 
 function Form(){
+    const [name, setName] = useState("")
+    const [newName, setNewName] = useState([])
+
+    function handleChange(event){
+        setName(prevName => ([...prevName, newName]))
+    }
     return(
         <div>
-            Its about that time!!!
+            <form>
+                <input type="text"
+                       placeholder="First Name"
+                       name="firstName"
+                       value=""
+                       onChange=""/>
+                <input type="text"
+                       placeholder="Last Name"
+                       name="lastName"
+                       value="lastName"
+                       onChange=""/>
+            </form>
         </div>
     )
 }
